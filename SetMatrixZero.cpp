4693d4b3580986626677771008853d1c60e134f5
tusharsharma2822 <tusharsharma2822@gmail.com>
2024-08-22 21:53:21 +0530

    Set Matrix Zero Soltion

--- SetMatrixZero.cpp
+++ SetMatrixZero.cpp
@@ -37,4 +37,41 @@ public:
     }
 };
 
-/* Note:- In the brute force solution, if any matrix will contain non zero numbers, especially -1, then it will also make it zero, which may lead to the failure of some of the testcases.*/
+/* Note:- In the brute force solution, if any matrix will contain non zero numbers, especially -1, then it will also make it zero, which may lead to the failure of some of the testcases.
+Time complexity - O(N^3)
+space complexity - O(1)
+*/
+
+/*Better Solution*/
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> rowArray(n,0);
+        vector<int> colArray(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j] == 0){
+                    rowArray[i] = 1;
+                    colArray[j] = 1;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(rowArray[i] == 1 || colArray[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+Note:- In the better solution, All the testcases that was failed in the brute force Solution will be passed.
+Time complexity of the better solution is (2*N*M) where N is the number of rows and M is the number of coloums.
+Space Complexity:- O(n) + O(m)
+
+/*Optimal Solution*/
