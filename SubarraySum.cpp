4702200e6e1d2f70d03d8c17d7753de9b731252c
tusharsharma2822 <tusharsharma2822@gmail.com>
2024-08-26 12:28:03 +0530

    Maximum Subarray Sum

--- SubarraySum.cpp
+++ SubarraySum.cpp
@@ -1,19 +1,56 @@
-/*Given an aaray of n N, find the maximum sum of a subarray*/
+// /*Given an aaray of n N, find the maximum sum of a subarray*/
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int maximumSubarraySum(int array[], int n){
-    int result = INT_MIN;
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            int sum = 0;
-            for(int k=i;k<j;k++){
-                sum += array[k];
-                result = max(sum,result);
-            }
+
+/*Brute Force:- Time Complexity = O(n^3) and Space Complexity = O(1)*/
+
+// int maximumSubarraySum(int array[], int n){
+//     int result = INT_MIN;
+//     for(int i=0;i<n;i++){
+//         for(int j=i;j<n;j++){
+//             int sum = 0;
+//             for(int k=i;k<j;k++){
+//                 sum += array[k];
+//                 result = max(sum,result);
+//             }
+//         }
+//     }
+//     return result;
+// }
+
+
+/*Better Solution:- Time Complexity = O(n^2) and Space Complexity = O(1)*/
+
+// int maximumSubarraySum(int array[], int size){
+//     int result = INT_MIN;
+//     for(int i=0;i<size;i++){
+//         int sum = 0;
+//         for(int j=i;j<size;j++){
+//             sum += array[j];
+//             result = max(sum,result);
+//         }
+//     }
+//     return result;
+// }
+
+/*Optimal Solution:- Kadane's Algorithm
+Time Complexity = O(n)
+Space Complexity = O(1) */
+
+int maximumSubarraySum(int array[], int size){
+    int result = 0;
+    int sum = 0;
+    for(int i=0;i<size;i++){
+        sum += array[i];
+        result = max(result,sum);
+
+        if(sum < 0){
+            sum = 0;
         }
     }
+
     return result;
 }
 
